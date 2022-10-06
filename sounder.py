4a8b55c8540b1e296c882793ad2474e112ebaa82
Michael Robinson <kb1dds@gmail.com>
2022-10-05 21:28:11 -0400

    Now works with GTK3+ and GStreamer 1.0

--- sounder.py
+++ sounder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # 1-d sonar sounder 
 
@@ -24,14 +24,13 @@
 #
 # Version 0.1
 
-import gtk
+import cairo
+import gi
+gi.require_version("Gtk","3.0")
+gi.require_version("Gst","1.0")
+from gi.repository import GObject, Gtk, Gdk, Gst, GLib
 
-import gobject
-import pygst
-pygst.require("0.10")
-import gst
 import time
-import pango
 import struct
 import wave
 import numpy
@@ -41,7 +40,6 @@ from numpy import conj
 from numpy.fft import fft, ifft
 from time import strftime
 
-# Unpack data from gstreamer
 def unpack(str,bytesPerSample=2):
     result=list()
     for i in range(0,len(str)-1,bytesPerSample):
@@ -57,33 +55,28 @@ class sounder:
         # gstreamer nicely.  I think there might be a deadlock since the tx pipeline 
         # gets re-seeked frequently.  In any event, it's apparently mostly OK to 
         # let the gc do the cleanup work for me, though this occasionally segfaults...
-#        self.rxpipeline.set_state(gst.STATE_PAUSED)
-#        self.txpipeline.set_state(gst.STATE_PAUSED)
-        gtk.main_quit()
-        
-    def update_display(self,event):
-        if self.updating:
-            return True
+        Gtk.main_quit()
 
-        self.updating=True
-        
+    def trigger_update(self):
+        rect=self.screen.get_allocation()
+        self.window.get_window().invalidate_rect(rect,True)
+        return True
+
+    def update_display(self,widget,ctx):
+       
         # Update the data area
-        self.dataBlock=numpy.concatenate((self.dataBlock[self.blockSize/2:],self.data))
+        self.dataBlock=numpy.concatenate((self.dataBlock[int(self.blockSize/2):],self.data))
 
         # Correlate against chirp reference
         if self.matchedcheck.get_active():
             self.dataBlock=ifft(fft(self.dataBlock)*self.ref*4/self.blockSize**2);
 
-        # Color state
-        style=self.screen.get_style()
-        gc=style.fg_gc[gtk.STATE_NORMAL]
-        gc_oldfg = gc.foreground
-
         # Erase current display
-        gc.foreground=self.black
-        self.pixmap.draw_rectangle(gc,True,0,0,self.screenWidth,self.screenHeight)
+        ctx.set_source_rgb(0,0,0)
+        ctx.rectangle(0,0,self.screenWidth,self.screenHeight)
+        ctx.fill()
 
-        gc.foreground = self.white
+        ctx.set_source_rgb(1,1,1)
 
         # Trigger if desired
         if self.centercheck.get_active():
@@ -91,27 +84,12 @@ class sounder:
         else:
             idx=0
 
-        # The following block of code should take the transmitted signal's clock for synchronizing the received signal.  But for some reason, it seems wildly inaccurate (roughly 0.017 s / pulse)
-#        maxRange=44 # meters
-#        pri=2*maxRange/340.0
-#        pos=self.captureTime/1e9-pri/2
-#        print(pos)
-#        prinum=numpy.floor(pos/pri)
-#        idx=round((pos-prinum*pri)*self.sampleRate)
-#        print(prinum)
-#        print(idx)
-#        
-#        if idx>self.blockSize/2*self.blocks or idx<0:
-#            idx=0
-
         # Align and downsample pulses
         if self.averagecheck.get_active():
             self.corr_data=numpy.roll(self.corr_data,1,axis=1)
-            #self.corr_data[0:numpy.size(self.corr_data,0)-idx,0]=abs(self.dataBlock[idx:])
             self.corr_data[:,0]=numpy.roll(abs(self.dataBlock),-idx,axis=0)
             plotdat=numpy.mean(self.corr_data[::self.get_step(),:],1)
         else:
-            #plotdat=self.dataBlock[idx::self.get_step()]
             self.corr_data=numpy.roll(self.dataBlock,-idx,axis=0)
             plotdat=self.corr_data[::self.get_step()]
 
@@ -122,26 +100,31 @@ class sounder:
         data[data<-10]=-10
         data[data>500]=500
         data=data+20
-        self.pixmap.draw_lines(gc,[(i2,int(self.screenHeight-e)) for i2,e in enumerate(data)])
+
+        # Plot the echo data
+        ctx.new_path()
+        ctx.move_to(0,int(self.screenHeight-data[0]))
+        for i2,e in enumerate(data):
+            ctx.line_to(i2,int(self.screenHeight-e))
+        ctx.stroke()            
         
         # Text labels
         for i in range(1,int(self.screenWidth/50)):
             y=i*50
             rangeMarker=self.pixels_to_cm(y)
-            layout=self.screen.create_pango_layout('%0.0f' % rangeMarker + ' cm')
-            gc.foreground=self.white
-            self.pixmap.draw_layout(gc,y,i*20,layout)
-            gc.foreground=self.green
-            self.pixmap.draw_line(gc,y,0,y,self.screenHeight)
 
-        # Restore default color
-        gc.foreground=gc_oldfg
+            ctx.set_source_rgb(1,1,1)
+            ctx.move_to(y,i*20)
+            ctx.set_font_size(12)
+            ctx.select_font_face('Arial', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+            ctx.show_text('%0.0f' % rangeMarker + ' cm')
+            
+            ctx.set_source_rgb(0,1,0)
+            ctx.new_path()
+            ctx.move_to(y,0)
+            ctx.line_to(y,self.screenHeight)
+            ctx.stroke()
 
-        # Update the window
-        self.screen.window.draw_drawable(self.screen.get_style().fg_gc[gtk.STATE_NORMAL],
-                                         self.pixmap, 0, 0, 0, 0, self.screenWidth, self.screenHeight)
-
-        self.updating=False
         return True
 
     # Set plotting interval (samples/pixel)
@@ -163,33 +146,34 @@ class sounder:
 
     ## GStreamer callbacks
     # Assemble the transmit pipeline as it gets linked together
-    def new_decoded_cb(self,bin,pad,islast):
+    def new_decoded_cb(self,element,pad):
         decode=pad.get_parent()
         pipeline=decode.get_parent()
-        convert=pipeline.get_by_name("ac2")
-        decode.link(convert)
-        pipeline.set_state(gst.STATE_PLAYING)
+        sink=pipeline.get_by_name("out")
+        pad.link(sink.get_static_pad('sink'))
 
     # Restart the txpipeline after on bank of pulses has been transmitted
     def tx_cb(self,bus,message):
         t=message.type
-        if t==gst.MESSAGE_EOS:
-            self.txpipeline.seek_simple(gst.FORMAT_TIME,gst.SEEK_FLAG_FLUSH, 0)
+        if t==Gst.MessageType.EOS:
+            self.txpipeline.seek_simple(Gst.Format.TIME,Gst.SeekFlags.FLUSH, 0)
         return True
 
     # Unpack data from gstreamer
-    def buffer_cb(self, buffer, pad, user_data):
-        #gtk.gdk.threads_enter()
-        #self.captureTime=self.txpipeline.query_position(gst.FORMAT_TIME)[0]
-        self.data=numpy.array([x*190.0/2.0**16 for x in unpack(pad)])
-        #gtk.gdk.threads_leave()
+    def buffer_cb(self, sink):
+        # Unpack and FFT data
+        sample=sink.emit('pull-sample')
+        buffer=sample.get_buffer()
+        self.data=numpy.frombuffer(buffer.extract_dup(0,buffer.get_size()),
+                                   dtype=numpy.int16)
 
-        return True
+        return Gst.FlowReturn.OK
+
 
     ## UI callbacks
     def average_cb(self,event):
         self.corr_data=[]
-        self.corr_data=numpy.zeros((self.blockSize/2*self.blocks,self.averagingWindow))
+        self.corr_data=numpy.zeros((int(self.blockSize/2*self.blocks),self.averagingWindow))
         return True
 
     def avg_up(self,event):
@@ -224,9 +208,10 @@ class sounder:
 
     def transmit_cb(self,event):
         if self.transmitcheck.get_active():
-            self.txpipeline.set_state(gst.STATE_PLAYING)
+            if self.txpipeline.set_state(Gst.State.PLAYING)  == Gst.StateChangeReturn.FAILURE:
+                print('oh no!')
         else:
-            self.txpipeline.set_state(gst.STATE_PAUSED)
+            self.txpipeline.set_state(Gst.State.PAUSED)
         return True
 
     def size_allocate_event(self,event,data=None):
@@ -239,24 +224,20 @@ class sounder:
             self.screenWidth+=data.width-self.width
 
             self.screen.set_size_request(self.screenWidth,self.screenHeight)
-            self.pixmap = gtk.gdk.Pixmap(self.screen.window, self.screenWidth, self.screenHeight)
-            self.pixmap.draw_rectangle(self.screen.get_style().black_gc,True,0,0,self.screenWidth,self.screenHeight)
             self.width=data.width
             self.height=data.height
         return True
 
     def __init__(self):
-        self.window = gtk.Window()
-
-        self.updating = False
+        self.window = Gtk.Window()
 
         # Transform parameters
         self.sampleRate=44100
         self.blockSize=3000*self.sampleRate/44100
         self.blocks=1
-        self.dataBlock=numpy.zeros((self.blocks*self.blockSize/2))
+        self.dataBlock=numpy.zeros(int(self.blocks*self.blockSize/2))
         self.averagingWindow=10
-        self.corr_data=numpy.zeros((self.blockSize/2*self.blocks,self.averagingWindow))
+        self.corr_data=numpy.zeros((int(self.blockSize/2*self.blocks),self.averagingWindow))
 
         # Window boilerplate
         self.window.set_title("Sounder")
@@ -266,14 +247,15 @@ class sounder:
         self.window.set_border_width(5)
         self.width=None
         self.height=None
-        self.window.set_geometry_hints(min_width=10,min_height=10)
 
         # Display area boilerplate
-        self.screen=gtk.DrawingArea()
+        self.screen=Gtk.DrawingArea()
         self.screenWidth=512
         self.screenHeight=380
         self.screen.set_size_request(self.screenWidth,self.screenHeight)
         self.zoom=1
+        self.screen.add_events( Gdk.EventMask.BUTTON_PRESS_MASK )
+        self.screen.connect("draw",self.update_display)
 
         # Load chirp reference
         f=wave.open("squeak.wav",'r')
@@ -281,78 +263,77 @@ class sounder:
         self.ref=numpy.conjugate(fft(wavdata,self.blockSize/2*self.blocks))
 
         # Construct gstreamer receiver pipeline to funnel data into the application
-        # pulsesrc ! audioconvert ! fakesink ! (this program)
-        self.rxpipeline=gst.Pipeline("rxpipeline")
+        # pulsesrc ! capsfilter ! appsink ! (this program)
+        self.rxpipeline=Gst.Pipeline.new("rxpipeline")
 
-        rxsrc=gst.element_factory_make("pulsesrc", "src")
+        rxsrc=Gst.ElementFactory.make("pulsesrc", "src")
+        rxsrc.set_property("blocksize",self.blockSize)
         self.rxpipeline.add(rxsrc)
 
-        ac=gst.element_factory_make("audioconvert","ac")
+        ac=Gst.ElementFactory.make("capsfilter","ac")
+        ac.set_property("caps",Gst.caps_from_string("audio/x-raw,format=S16LE,rate="+ str(self.sampleRate) + ",channels=1"))
         self.rxpipeline.add(ac)
 
-        sink=gst.element_factory_make("fakesink","fakesink")
+        sink=Gst.ElementFactory.make("appsink","as")
+        sink.set_property('max-buffers',20)
+        sink.set_property("emit-signals",True)
+        sink.set_property("sync",False)
+        sink.connect("new-sample",self.buffer_cb)
         self.rxpipeline.add(sink)
 
-        rxsrc.link(ac,gst.caps_from_string("audio/x-raw-int,width=16,signed=true,rate="+ str(self.sampleRate) + ",channels=1"))
-        rxsrc.set_property("blocksize",self.blockSize)
+        rxsrc.link(ac)
         ac.link(sink)
-        sink.set_property("signal-handoffs",True)
-        sink.connect("handoff",self.buffer_cb)
 
         # Construct gstreamer pipeline for transmission
-        self.txpipeline=gst.Pipeline("txpipeline")
+        self.txpipeline=Gst.Pipeline.new("txpipeline")
         
-        txsrc=gst.element_factory_make("filesrc","src2")
+        txsrc=Gst.ElementFactory.make("filesrc","txsrc")
         txsrc.set_property("location","squeaks.wav")
         self.txpipeline.add(txsrc)
 
-        dec=gst.element_factory_make("decodebin","wd")
-        self.txpipeline.add(dec)
-        txsrc.link(dec)
-        dec.connect("new-decoded-pad", self.new_decoded_cb)
+        wd=Gst.ElementFactory.make("decodebin","wd")
+        self.txpipeline.add(wd)
+        txsrc.link(wd)
+        wd.connect("pad-added", self.new_decoded_cb)
 
-        ac2=gst.element_factory_make("audioconvert","ac2")
-        self.txpipeline.add(ac2)
-
-        sink2=gst.element_factory_make("pulsesink","out")
+        sink2=Gst.ElementFactory.make("pulsesink","out")
         self.txpipeline.add(sink2)
-        ac2.link(sink2)
 
         bus=self.txpipeline.get_bus()
         bus.add_signal_watch()
         bus.connect("message",self.tx_cb)
 
         # Organization on window...
-        hbox=gtk.HBox(False,10)
+        hbox=Gtk.HBox(homogeneous=False,spacing=10)
         hbox.pack_start(self.screen,False,True,0)
         
-        vbox=gtk.VBox(True,0)
-        self.matchedcheck=gtk.CheckButton('Matched filter')
+        vbox=Gtk.VBox(homogeneous=True,spacing=0)
+        self.matchedcheck=Gtk.CheckButton(label='Matched filter')
         self.matchedcheck.set_active(True)
         self.matchedcheck.connect('toggled',self.average_cb)
         vbox.pack_start(self.matchedcheck,True,True,0)
-        self.averagecheck=gtk.CheckButton('Averaging')
+        self.averagecheck=Gtk.CheckButton(label='Averaging')
         self.averagecheck.set_active(True)
         self.averagecheck.connect('toggled',self.average_cb)
         vbox.pack_start(self.averagecheck,True,True,0)
-        self.transmitcheck=gtk.CheckButton('Transmit')
-        self.transmitcheck.set_active(True)
+        self.transmitcheck=Gtk.CheckButton(label='Transmit')
+        self.transmitcheck.set_active(False)
         self.transmitcheck.connect('toggled',self.transmit_cb)
         vbox.pack_start(self.transmitcheck,True,True,0)
-        self.centercheck=gtk.CheckButton('Center');
+        self.centercheck=Gtk.CheckButton(label='Center');
         self.centercheck.set_active(True)
         vbox.pack_start(self.centercheck,True,True,0)
 
-        hbox2=gtk.HBox(True,0)
-        button=gtk.Button('+')
+        hbox2=Gtk.HBox(homogeneous=True,spacing=0)
+        button=Gtk.Button(label='+')
         button.connect('clicked',self.avg_up)
         hbox2.pack_start(button,True,True,0)
-        button=gtk.Button('-')
+        button=Gtk.Button(label='-')
         button.connect('clicked',self.avg_dn)
         hbox2.pack_start(button,True,True,0)
         vbox.pack_end(hbox2,True,True,0)
 
-        button=gtk.Button('Save')
+        button=Gtk.Button(label='Save')
         button.connect('clicked',self.saveButton)
         vbox.pack_end(button,True,True,0)
 
@@ -362,33 +343,23 @@ class sounder:
         self.window.add(hbox)
         self.window.show_all()
 
-        # Backing pixmap for the display
-        self.pixmap = gtk.gdk.Pixmap(self.screen.window, self.screenWidth, self.screenHeight)
-        self.pixmap.draw_rectangle(self.screen.get_style().black_gc,True,0,0,self.screenWidth,self.screenHeight)
-
-        # Reference color data
-        self.black=self.screen.window.get_colormap().alloc_color(0,0,0)
-        self.white=self.screen.window.get_colormap().alloc_color(0xFFFF,0xFFFF,0xFFFF)
-        self.red=self.screen.window.get_colormap().alloc_color(0xFFFF,0,0)
-        self.green=self.screen.window.get_colormap().alloc_color(0,0xFFFF,0)
-        self.blue=self.screen.window.get_colormap().alloc_color(0,0,0xFFFF)
-
         # Turn on receiver chain
-        self.rxpipeline.set_state(gst.STATE_PLAYING) 
+        self.rxpipeline.set_state(Gst.State.PLAYING) 
 
         # Wait to start transmitting until the pipeline is fully assembled
-        self.txpipeline.set_state(gst.STATE_PAUSED)  
+        self.txpipeline.set_state(Gst.State.PAUSED)  
 
         self.data=numpy.zeros(self.dataBlock.shape)
-        gobject.idle_add(self.update_display,self)
-        
+
+        self.data=numpy.zeros(self.dataBlock.shape)
+        GLib.timeout_add(50,self.trigger_update)
         return
 
     def main(self):
-        gtk.main()
+        Gtk.main()
         return 0
 
 if __name__ == "__main__":
-    gtk.gdk.threads_init()
+    Gst.init(None)
     sounderob=sounder()
     sounderob.main()
